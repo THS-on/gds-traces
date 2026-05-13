9a81e4be079b60f4e31a45e64ebaa037b42c26e9
Thore Sommer <thore.sommer@tum.de>
2026-05-13 10:38:11 +0200

    flake: add pre-commit hooks

--- flake.nix
+++ flake.nix
@@ -3,17 +3,42 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
   };
 
-  outputs = { self, nixpkgs }:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      pre-commit-hooks,
+    }:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
       python = pkgs.python3.withPackages (ps: [ ps.invoke ]);
+
     in
     {
+      formatter.${system} = pkgs.nixfmt-rfc-style;
+
+      checks.${system}.pre-commit-check = pre-commit-hooks.lib.${system}.run {
+        src = ./.;
+        hooks = {
+          nixfmt-rfc-style.enable = true;
+          ruff.enable = true;
+          ruff-format.enable = true;
+        };
+      };
+
       devShells.${system}.default = pkgs.mkShell {
-        packages = [ python pkgs.e2fsprogs pkgs.util-linux ];
+        packages = [
+          python
+          pkgs.e2fsprogs
+          pkgs.util-linux
+        ];
+        shellHook = ''
+          ${self.checks.${system}.pre-commit-check.shellHook}
+        '';
       };
     };
 }
