172751fe6ab9b466976241dd3a72fff20430ca7f
Thore Sommer <thore.sommer@tum.de>
2026-05-15 16:17:16 +0200

    flake: add perf, fio, use nixfmt

--- flake.nix
+++ flake.nix
@@ -16,15 +16,20 @@
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
       python = pkgs.python3.withPackages (ps: [ ps.invoke ]);
-
+      hostname = pkgs.lib.strings.trim (builtins.readFile /etc/hostname);
+      nixosFlake = builtins.getFlake (
+        builtins.unsafeDiscardStringContext (builtins.storePath "/run/booted-system/flake")
+      );
+      kernel = nixosFlake.outputs.nixosConfigurations.${hostname}.config.boot.kernelPackages.kernel;
     in
     {
-      formatter.${system} = pkgs.nixfmt-rfc-style;
+
+      formatter.${system} = pkgs.nixfmt;
 
       checks.${system}.pre-commit-check = pre-commit-hooks.lib.${system}.run {
         src = ./.;
         hooks = {
-          nixfmt-rfc-style.enable = true;
+          nixfmt.enable = true;
           ruff.enable = true;
           ruff-format.enable = true;
         };
@@ -35,6 +40,8 @@
           python
           pkgs.e2fsprogs
           pkgs.util-linux
+          pkgs.fio
+          pkgs.perf
         ];
         shellHook = ''
           ${self.checks.${system}.pre-commit-check.shellHook}
