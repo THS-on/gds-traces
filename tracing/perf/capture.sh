20e17d8dc1e51c3d1268d51d24a3c88d3c7acf63
Thore Sommer <thore.sommer@tum.de>
2026-05-15 15:56:20 +0200

    sync

--- tracing/perf/capture.sh
+++ tracing/perf/capture.sh
@@ -13,4 +13,4 @@ sudo perf record \
 
 sudo perf inject --build-ids -i nvme_raw.data -o nvme_sorted.data
 
-sudo perf script -i nvme_sorted.data -s perf-parse-sgl-prp.py
+sudo perf script -i nvme_sorted.data -s nvme_trace_events.py
