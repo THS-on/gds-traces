e3af172dd7522789e4542d3d98a30bfc2a3e1ca2
Thore Sommer <thore.sommer@tum.de>
2026-05-15 15:09:53 +0200

    sync

--- tracing/perf/capture.sh
+++ tracing/perf/capture.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 
 sudo perf record \
-    -e nvme:nvme_pci_submit \
-    -e nvme:nvme_pci_complete \
+    -e nvme:nvme_pci_submit --filter 'ctrl_id==1' \
+    -e nvme:nvme_pci_complete --filter 'ctrl_id==1' \
     -a \
     -T \
     --sample-identifier \
     -k CLOCK_MONOTONIC \
-    -o nvme_raw.data
+    -o nvme_raw.data \ 
+    fio --name=nvme_write --filename=/dev/disk/by-id/nvme-KIOXIA_KCMYXRUG3T84_4FB0A0CT0LM3_1 --rw=write --bs=128k --iodepth=4 --ioengine=libaio --direct=1 --size=200G --numjobs=1
 
 sudo perf inject --build-ids -i nvme_raw.data -o nvme_sorted.data
 
-perf script -i nvme_sorted.data -s perf-parse-sgl-prp.py
+sudo perf script -i nvme_sorted.data -s perf-parse-sgl-prp.py
